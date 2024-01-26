f9104d4e6c4fb093df3f01cfd7a3b07b653f1b67
MaWenHao <377230216@qq.com>
2024-01-26 14:55:21 +0800

    ota：串口处理开发中

--- examples/Lm-Study/ssl_mutual_auth/main/app_main.c
+++ examples/Lm-Study/ssl_mutual_auth/main/app_main.c
@@ -33,21 +33,230 @@
 #include "network_http.h"
 #include "tuya_sdk.h"
 #include "cJSON.h"
-#include "uart.h"
-static const char *TAG = "MAIN";
+#include "driver/uart.h"
+#define UART_MAX_NUM_RX_BYTES (255+4)
 
-/*********************************************************************
- * PUBLIC FUNCTIONS
- */
-/**
- @brief 串口驱动初始化
- @param 无
- @return 无
-*/
-void UART_Init(void)
+static const char *TAG = "MAIN";
+#define BUF_SIZE (1024)
+
+int user_ceil(float num) {
+    int inum = (int)num;
+    if (num == (float)inum) {
+        return inum;
+    }
+    return inum + 1;
+}
+
+
+
+
+
+// ------------------------- cmd_id -------------------------
+// 0x00 双方通用的应答包 数据长度 1 数据区 0xff nack
+// 0x01 esp询问stm32的版本信息                  nack
+// 0x02 stm32给esp发送版本信息                  nack
+// 0x03 esp发送给stm32 进入bool模式             ack
+// 0x04 stm32发送esp 已经处于bool模式           nack
+// 0x05 esp将固件拆分成N/128分发送到stm32       ack
+// ------------------------- cmd_id -------------------------
+
+// 串口帧头
+#define UART_DATA_HEAD_1 0xED
+#define UART_DATA_HEAD_2 0x90
+// 串口帧头
+// 是否需要应答 发送方发送ack 接收方需要回传应答包 否则重新发送
+// static char uart_isack = 0;
+#define UART_DATA_ACK_3  0X01 
+#define UART_DATA_NACK_3 0X02
+// 是否需要应答
+
+// 数据长度
+// static char UART_DATA_LENGTH = 0;
+// 数据长度
+
+// 数据区
+// 固件分包 第几个分包
+// static char uart_firmware_count = 0;
+// 固件分包 第几个分包
+// ....
+// 数据区
+
+// 校验和 整条数据做校验
+// static char uart_data_sum_high = 0;
+// static char uart_data_sum_low  = 0;
+// 校验和 整条数据做校验
+
+
+// 计算 CRCCRC-16/MODBUS
+// crc modbus 校验和遵循 低位在前 高位在后
+// https://blog.csdn.net/m0_37697335/article/details/113267780
+uint16_t calculateCRC(char *data, int length) {
+    unsigned int i;
+    unsigned short crc = 0xFFFF;  //crc16位寄存器初始值
+    printf("crc16_modbus校验->length:%d data:",length);
+    while(length--)
+    {
+        printf("%02x ",*data);
+        crc ^= *data++;
+        for (i = 0; i < 8; ++i)
+        {
+            if (crc & 1)
+            {
+                crc = (crc >> 1) ^ 0xA001; //多项式 POLY（0x8005)的高低位交换值，这是由于其模型的一些参数决定的
+            }
+            else
+            {
+                crc = (crc >> 1);
+            }
+        }
+    }
+    printf("\r\n");
+    return crc;
+}
+// uart_data 传入一个空指针进来
+// cmd_id 命令id
+// isack 是否应答
+// data 数据区
+// data_length 数据区的长度
+// void uart_send_cmdid_data_handle(char *uart_rx_data,uint8_t cmd_id, uint8_t isack, char* data, int data_length)
+// {
+//     int uart_rx_data_length = data_length + 6;
+//     uart_rx_data = malloc( uart_rx_data_length * sizeof(char));
+
+//     uart_rx_data[0] = UART_DATA_HEAD_1;
+//     uart_rx_data[1] = UART_DATA_HEAD_2;
+//     uart_rx_data[2] = cmd_id;         
+//     uart_rx_data[3] = isack;         
+//     char *data_ = &(uart_rx_data[5]);
+// }
+// 开始分批发送固件包
+esp_err_t ota_send_firmware(http_files_data *hf_data)
 {
-	// Configure parameters of an UART driver,
-    // communication pins and install the driver
+    // 固件拆分次数
+    char* firmware = (char*)hf_data->data;
+    int firmware_size = hf_data->readData_count;
+    // memset(firmware, 0x66, firmware_size);
+    int firmware_split_number = (int)user_ceil((double)((double)firmware_size / 128.0000));
+
+
+
+    char *uart_rx_data = NULL;
+    int uart_rx_data_length = 0;
+    for (int i = 0; i < firmware_split_number; i++)
+    {
+       	int firmware_block_size = 0;	// 固件包大小
+		printf("第%d次拆分->固件当前索引位:%d", i, (i * 128));
+		if(firmware_size - (i*128) > 128 )
+		{
+			firmware_block_size = 128;
+		}
+		else{
+			firmware_block_size = firmware_size - ((i)*128);
+		}
+		printf("分包大小：%d\r\n",firmware_block_size);
+        // 固件块大小+7帧头什么的
+        uart_rx_data_length = (firmware_block_size + 7) ;
+        uart_rx_data = malloc( uart_rx_data_length * sizeof(char));
+        memset(uart_rx_data,0,uart_rx_data_length * sizeof(char));
+
+        // 先拼接一下
+        uart_rx_data[0] = UART_DATA_HEAD_1;
+        uart_rx_data[1] = UART_DATA_HEAD_2;
+        uart_rx_data[2] = UART_DATA_ACK_3;            // 需要应答   
+        uart_rx_data[3] = firmware_block_size + 1;    // 数据区的长度
+        uart_rx_data[4] = i;                          // 第几个固件包 uart_firmware_count
+
+        char *data_ = &(uart_rx_data[5]);
+	    memcpy(data_, (char *)(firmware+(i * 128)) ,  firmware_block_size * sizeof(char));
+                                                        // -2 是要把校验位剪了
+        uint16_t crc = calculateCRC(uart_rx_data, uart_rx_data_length - 2);
+
+
+        uart_rx_data[5+firmware_block_size+0] =  crc & 0xFF;        // 校验和 低位
+        uart_rx_data[5+firmware_block_size+1] = (crc >> 8) & 0xFF;  // 校验和 高位
+
+        uart_write_bytes(UART_NUM_0, uart_rx_data, uart_rx_data_length);
+        
+	    // 输出固件debug
+        printf("串口命令debug:");
+		for(int i = 0; i < uart_rx_data_length; i++){
+			printf("%02x ",((char*)uart_rx_data)[i]);
+		}
+		printf("\r\n");
+		// 释放分配的内存
+        free(uart_rx_data);
+    }
+    return ESP_OK;
+}
+
+
+
+
+// ------------------------- cmd_id -------------------------
+// 0x00 双方通用的应答包 数据长度 1 数据区 0xff nack
+// 0x01 esp询问stm32的版本信息                  nack
+// 0x01 stm32应答版本信息                       nack
+// 0x02 esp让stm进入bool模式。                  ack
+// 0x02 stm进入bool后发送esp已经进入            ack
+// 0x03 esp将固件拆分成N/128分发送到stm32       ack
+// ------------------------- cmd_id -------------------------
+
+void ota_uart_data_handle(char *uart_data, int length)
+{
+    ESP_LOGI("ota_uart:","length:%d",length);
+    if(uart_data[0] != UART_DATA_HEAD_1 || uart_data[1] != UART_DATA_HEAD_2)
+    {
+        ESP_LOGE("ota_uart:","串口ota数据包头异常\r\n");
+        return;
+    }
+    // 通过串口包的数据计算出来的校验和
+    uint16_t analysis_crc = calculateCRC(uart_data, length - 2); // 将读取到的数据计算校验和
+    uint8_t analysis_crc_low = (analysis_crc & 0xFF);
+    uint8_t analysis_crc_high = ((analysis_crc >> 8) & 0xFF);
+    // 从串口包读取的校验和
+    uint8_t uart_crc_low = uart_data[length-2];
+    uint8_t uart_crc_high = uart_data[length-1];
+    // ESP_LOGI("ota_uart:","通过串口数据算出来的校验和 %02x %02x",  analysis_crc_low , analysis_crc_high);
+    // ESP_LOGI("ota_uart:","读取串口收到的校验和 %02x %02x",  uart_crc_low, uart_crc_high);
+
+    if(analysis_crc_low != uart_crc_low || analysis_crc_high != uart_crc_high)
+    {
+        ESP_LOGE("ota_uart:","校验和异常\r\n");
+        return;
+    }
+
+    // char *cmd_id = &data[2];
+    // char *data_length = &data[3];   // 数据区的长度
+    // char *data_ = &data[4];         // 数据区的数据
+    // // uint16_t read_crc =  ((uint16_t)data[data_length] << 8) | data[data_length+1];
+    // // uint16_t crc = calculateCRC(data_, (int)data_length);
+    // ESP_LOGI("ota_uart:","cmdid:%02x data_length:%02x read_crc_gao:%02x read_crc_di:%02x",
+    //     *cmd_id,*data_length,*(data_),*(data_+1)
+    //     );
+    // switch (*cmd_id)
+    // {
+    //     case 0x00:
+    //             ESP_LOGE("ota_uart:","双方通用的应答包命令\r\n"); 
+    //         break;
+    //     case 0x01:
+    //             ESP_LOGE("ota_uart:","版本信息命令\r\n"); 
+    //         break;
+    //     case 0x02:
+    //             ESP_LOGE("ota_uart:","bool模式命令\r\n"); 
+    //         break;
+    //     case 0x03:
+    //             //  固件包命令固件包 data[data_length] = 第几个固件包 
+    //             // data[data_length+1]开始才是固件数据
+    //             ESP_LOGE("ota_uart:","固件包命令\r\n"); 
+    //         break;
+    //     default:
+    //         break;
+    // }
+}
+
+static void uart_select_task()
+{
+    // 串口基础配置
     uart_config_t uart_config = 
 	{
         .baud_rate = 115200,
@@ -56,14 +265,34 @@ void UART_Init(void)
         .stop_bits = UART_STOP_BITS_1,
         .flow_ctrl = UART_HW_FLOWCTRL_DISABLE
     };
+    uart_param_config(UART_NUM_0, &uart_config);
+    uart_driver_install(UART_NUM_0, BUF_SIZE * 2, 0, 0, NULL, 0);
 
-    uart_param_config(UART_NUM_0, &uart_config);											// 配置串口0参数
+    // Configure a temporary buffer for the incoming data
+    uint8_t *data_u = (uint8_t *) malloc(BUF_SIZE);
 
-    // Install UART driver, and get the queue.
-    uart_driver_install(UART_NUM_0, BUF_SIZE * 2, BUF_SIZE * 2, 100, &s_uart0Queue, 0);		// 安装UART驱动程序
+    while (1) {
+        // Read data from the UART
+        int len = uart_read_bytes(UART_NUM_0, data_u, BUF_SIZE, 20 / portTICK_RATE_MS);
+        // // Write data back to the UART
+        // uart_write_bytes(UART_NUM_0, (const char *) data, len);
+        if(len > 0)
+        {
+            ota_uart_data_handle((char*)data_u, len);
+        }
+    }
 
-    // Create a task to handler UART event from ISR
-    xTaskCreate(uartEventTask, "uartEventTask", 2048, NULL, 12, NULL);	
+    vTaskDelete(NULL);
+}
+/**
+ @brief 串口驱动初始化
+ @param 无
+ @return 无
+*/
+void UART_Init(void)
+{
+    // 创建一个uart任务处理事件
+    xTaskCreate(uart_select_task, "uart_select_task", 4*1024, NULL, 5, NULL);
 }
 
 void app_main(void)
@@ -82,12 +311,13 @@ void app_main(void)
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
-
+    UART_Init();
     ESP_ERROR_CHECK(example_connect());
 
     http_files_data *myData;
     myData = malloc(sizeof(http_files_data)); // 为结构体指针分配内存
     http_dowm_files(myData,"http://airtake-public-data-1254153901.cos.tuyacn.com/smart/firmware/upgrade/bay1705565836444xBls/17059072757e724aa563f.bin");
+    ota_send_firmware(myData);
 
     mqtt_app_start();
 }
